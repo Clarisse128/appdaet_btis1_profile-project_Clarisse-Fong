b918084de9a463e87728514a508fffe146f51947
Clarisse128 <clarisse.fong@benilde.edu.ph>
2026-05-22 20:17:36 +0800

    Update profile with skills list and graduation calculation

---
+++ profile2.py
@@ -0,0 +1,9 @@
+# Appending new collection structures and calculations
+
+skills = ["Python", "Git", "HTML"]  # List collection type
+
+years_to_graduation = 4 - 1.5  # Float calculation evaluation
+
+print("Skills:", skills)
+
+print("Years left to graduate:", years_to_graduation)
